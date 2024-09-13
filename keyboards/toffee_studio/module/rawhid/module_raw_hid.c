6e4395ce332775b4e68b4ba01c197ea2ca654c2d
Helmut Lord <kellyhlord@gmail.com>
2024-09-12 20:46:52 -0400

    raw hid parsing rework, fix buffer overflow

--- keyboards/toffee_studio/module/rawhid/module_raw_hid.c
+++ keyboards/toffee_studio/module/rawhid/module_raw_hid.c
@@ -8,6 +8,42 @@
 #include "lvgl.h"
 
 #define DIRECTORY_MAX 64
+#define MAX_PATH_LENGTH 256
+
+// Function declarations
+static int parse_ls(uint8_t *data, uint8_t length);
+static int parse_cd(uint8_t *data, uint8_t length);
+static int parse_pwd(uint8_t *data, uint8_t length);
+static int parse_rm(uint8_t *data, uint8_t length);
+static int parse_mkdir(uint8_t *data, uint8_t length);
+static int parse_touch(uint8_t *data, uint8_t length);
+static int parse_cat(uint8_t *data, uint8_t length);
+static int parse_open(uint8_t *data, uint8_t length);
+static int parse_write(uint8_t *data, uint8_t length);
+static int parse_close(uint8_t *data, uint8_t length);
+static int parse_format_filesystem(uint8_t *data, uint8_t length);
+static int parse_flash_remaining(uint8_t *data, uint8_t length);
+static int parse_choose_image(uint8_t *data, uint8_t length);
+static int parse_write_display(uint8_t *data, uint8_t length);
+static int parse_set_time(uint8_t *data, uint8_t length);
+
+static module_raw_hid_parse_t* parse_packet_funcs[] = {
+    parse_ls,
+    parse_cd,
+    parse_pwd,
+    parse_rm,
+    parse_mkdir,
+    parse_touch,
+    parse_cat,
+    parse_open,
+    parse_write,
+    parse_close,
+    parse_format_filesystem,
+    parse_flash_remaining,
+    parse_choose_image,
+    parse_write_display,
+    parse_set_time,
+};
 
 lfs_file_t current_file;
 char current_directory[DIRECTORY_MAX];
@@ -71,6 +107,12 @@ static int parse_ls(uint8_t *data, uint8_t length) {
         }
 
         int name_len = strlen(info.name);
+
+        // Ensure name_len does not exceed buffer size
+        if (name_len > RAW_EPSIZE - offset - 2) {
+            name_len = RAW_EPSIZE - offset - 2;
+        }
+
         if (offset + name_len + 2 > RAW_EPSIZE) {
             raw_hid_send(return_buf, RAW_EPSIZE);
             memset(return_buf + 1, 0, RAW_EPSIZE - 1);  // Clear all but the first byte
@@ -79,7 +121,7 @@ static int parse_ls(uint8_t *data, uint8_t length) {
 
         strncpy((char *)return_buf + offset, info.name, name_len);
         offset += name_len;
-        return_buf[offset++] = info.type == LFS_TYPE_DIR ? '/' : ' ';
+        return_buf[offset++] = (info.type == LFS_TYPE_DIR) ? '/' : ' ';
         return_buf[offset++] = '\n';
     }
 
@@ -95,8 +137,21 @@ static int parse_ls(uint8_t *data, uint8_t length) {
 static int parse_cd(uint8_t *data, uint8_t length) {
     uprintf("Change directory\n");
 
-    struct cd_packet *cd_data = (struct cd_packet *)data;
-    char *new_directory = (char *)cd_data->header.packet_id;
+    if (length <= sizeof(struct packet_header)) {
+        uprintf("Insufficient data length\n");
+        return module_ret_invalid_command;
+    }
+
+    uint8_t *path_data = data + sizeof(struct packet_header);
+    size_t path_length = length - sizeof(struct packet_header);
+
+    char new_directory[DIRECTORY_MAX];
+    if (path_length >= DIRECTORY_MAX) {
+        uprintf("Directory name too long\n");
+        return module_ret_invalid_command;
+    }
+    memcpy(new_directory, path_data, path_length);
+    new_directory[path_length] = '\0';
 
     lfs_dir_t dir;
     int err = lfs_dir_open(&lfs, &dir, new_directory);
@@ -107,8 +162,9 @@ static int parse_cd(uint8_t *data, uint8_t length) {
 
     lfs_dir_close(&lfs, &dir);
 
-    // Update current directory (assuming you have a global variable for this)
-    // strncpy(current_directory, new_directory, LFS_NAME_MAX);
+    // Update current directory
+    strncpy(current_directory, new_directory, DIRECTORY_MAX - 1);
+    current_directory[DIRECTORY_MAX - 1] = '\0';
 
     uprintf("Changed to directory: %s\n", new_directory);
     return module_ret_success;
@@ -117,11 +173,19 @@ static int parse_cd(uint8_t *data, uint8_t length) {
 static int parse_pwd(uint8_t *data, uint8_t length) {
     uprintf("Print working directory\n");
 
-    // Assuming you have a global variable for current directory
-    // uprintf("Current directory: %s\n", current_directory);
-
     uint8_t return_buf[RAW_EPSIZE] = {0};
-    // strncpy((char *)return_buf, current_directory, RAW_EPSIZE - 1);
+    return_buf[0] = module_ret_success;
+
+    // Ensure current_directory is null-terminated
+    current_directory[DIRECTORY_MAX - 1] = '\0';
+    size_t dir_length = strlen(current_directory);
+
+    if (dir_length > RAW_EPSIZE - 1) {
+        dir_length = RAW_EPSIZE - 1;
+    }
+
+    memcpy(return_buf + 1, current_directory, dir_length);
+
     raw_hid_send(return_buf, RAW_EPSIZE);
 
     return module_ret_success;
@@ -130,8 +194,21 @@ static int parse_pwd(uint8_t *data, uint8_t length) {
 static int parse_rm(uint8_t *data, uint8_t length) {
     uprintf("Remove file/directory\n");
 
-    struct rm_packet *rm_data = (struct rm_packet *)data;
-    char *path = (char *)rm_data->directory;
+    if (length <= sizeof(struct packet_header)) {
+        uprintf("Insufficient data length\n");
+        return module_ret_invalid_command;
+    }
+
+    uint8_t *path_data = data + sizeof(struct packet_header);
+    size_t path_length = length - sizeof(struct packet_header);
+
+    char path[MAX_PATH_LENGTH];
+    if (path_length >= MAX_PATH_LENGTH) {
+        uprintf("Path too long\n");
+        return module_ret_invalid_command;
+    }
+    memcpy(path, path_data, path_length);
+    path[path_length] = '\0';
 
     int err = lfs_remove(&lfs, path);
     if (err < 0) {
@@ -145,8 +222,21 @@ static int parse_rm(uint8_t *data, uint8_t length) {
 static int parse_mkdir(uint8_t *data, uint8_t length) {
     uprintf("Make directory\n");
 
-    struct mkdir_packet *mkdir_data = (struct mkdir_packet *)data;
-    char *path = (char *)mkdir_data->directory;
+    if (length <= sizeof(struct packet_header)) {
+        uprintf("Insufficient data length\n");
+        return module_ret_invalid_command;
+    }
+
+    uint8_t *path_data = data + sizeof(struct packet_header);
+    size_t path_length = length - sizeof(struct packet_header);
+
+    char path[MAX_PATH_LENGTH];
+    if (path_length >= MAX_PATH_LENGTH) {
+        uprintf("Path too long\n");
+        return module_ret_invalid_command;
+    }
+    memcpy(path, path_data, path_length);
+    path[path_length] = '\0';
 
     int err = lfs_mkdir(&lfs, path);
     if (err < 0) {
@@ -160,17 +250,32 @@ static int parse_mkdir(uint8_t *data, uint8_t length) {
 static int parse_touch(uint8_t *data, uint8_t length) {
     uprintf("Create empty file\n");
 
-    struct touch_packet *touch_data = (struct touch_packet *)data;
-    char *path = (char *)touch_data->directory;
+    if (length <= sizeof(struct packet_header)) {
+        uprintf("Insufficient data length\n");
+        return module_ret_invalid_command;
+    }
+
+    uint8_t *path_data = data + sizeof(struct packet_header);
+    size_t path_length = length - sizeof(struct packet_header);
+
+    char path[MAX_PATH_LENGTH];
+    if (path_length >= MAX_PATH_LENGTH) {
+        uprintf("Path too long\n");
+        return module_ret_invalid_command;
+    }
+    memcpy(path, path_data, path_length);
+    path[path_length] = '\0';
 
     lfs_file_t file;
     int err = lfs_file_open(&lfs, &file, path, LFS_O_WRONLY | LFS_O_CREAT);
     if (err < 0) {
+        uprintf("Error creating file: %d\n", err);
         return err;
     }
 
     err = lfs_file_close(&lfs, &file);
     if (err < 0) {
+        uprintf("Error closing file: %d\n", err);
         return err;
     }
 
@@ -180,8 +285,21 @@ static int parse_touch(uint8_t *data, uint8_t length) {
 static int parse_cat(uint8_t *data, uint8_t length) {
     uprintf("Read file contents\n");
 
-    struct cat_packet *cat_data = (struct cat_packet *)data;
-    char *path = (char *)cat_data->directory;
+    if (length <= sizeof(struct packet_header)) {
+        uprintf("Insufficient data length\n");
+        return module_ret_invalid_command;
+    }
+
+    uint8_t *path_data = data + sizeof(struct packet_header);
+    size_t path_length = length - sizeof(struct packet_header);
+
+    char path[MAX_PATH_LENGTH];
+    if (path_length >= MAX_PATH_LENGTH) {
+        uprintf("Path too long\n");
+        return module_ret_invalid_command;
+    }
+    memcpy(path, path_data, path_length);
+    path[path_length] = '\0';
 
     lfs_file_t file;
     int err = open_file(&lfs, &file, path, LFS_O_RDONLY);
@@ -192,13 +310,8 @@ static int parse_cat(uint8_t *data, uint8_t length) {
     uint8_t return_buf[RAW_EPSIZE] = {0};
     lfs_ssize_t bytes_read;
     while ((bytes_read = lfs_file_read(&lfs, &file, return_buf + 1, RAW_EPSIZE - 1)) > 0) {
-        uprintf("Read %d bytes\n", bytes_read);
-        uprintf("File %s\n", path);
-        for (int i = 0; i < RAW_EPSIZE; i++) {
-            uprintf("%x ", return_buf[i]);
-        }
-        uprintf("\n");
-        return_buf[0] = 0;
+        uprintf("Read %d bytes from file %s\n", bytes_read, path);
+        return_buf[0] = module_ret_success;
         raw_hid_send(return_buf, RAW_EPSIZE);
     }
 
@@ -219,8 +332,21 @@ static int parse_cat(uint8_t *data, uint8_t length) {
 static int parse_open(uint8_t *data, uint8_t length) {
     uprintf("Open file\n");
 
-    struct open_packet *open_data = (struct open_packet *)data;
-    char *path = (char *)open_data->directory;
+    if (length <= sizeof(struct packet_header)) {
+        uprintf("Insufficient data length\n");
+        return module_ret_invalid_command;
+    }
+
+    uint8_t *path_data = data + sizeof(struct packet_header);
+    size_t path_length = length - sizeof(struct packet_header);
+
+    char path[MAX_PATH_LENGTH];
+    if (path_length >= MAX_PATH_LENGTH) {
+        uprintf("Path too long\n");
+        return module_ret_invalid_command;
+    }
+    memcpy(path, path_data, path_length);
+    path[path_length] = '\0';
 
     int err = open_file(&lfs, &current_file, path, LFS_O_WRONLY | LFS_O_CREAT | LFS_O_APPEND);
     if (err < 0) {
@@ -233,15 +359,22 @@ static int parse_open(uint8_t *data, uint8_t length) {
 static int parse_write(uint8_t *data, uint8_t length) {
     uprintf("Write to file\n");
 
-    struct write_packet *write_data = (struct write_packet *)data;
+    if (length <= sizeof(struct packet_header)) {
+        uprintf("No data to write\n");
+        return module_ret_invalid_command;
+    }
 
-     lfs_ssize_t written = lfs_file_write(&lfs, &current_file, write_data->data, length - sizeof(struct packet_header));
-     if (written < 0) {
-         uprintf("Error writing to file: %d\n", written);
-         return written;
-     }
+    uint8_t *write_data = data + sizeof(struct packet_header);
+    size_t data_length = length - sizeof(struct packet_header);
+
+    lfs_ssize_t written = lfs_file_write(&lfs, &current_file, write_data, data_length);
+    if (written < 0) {
+        uprintf("Error writing to file: %d\n", written);
+        return written;
+    }
 
     uint8_t return_buf[RAW_EPSIZE] = {0};
+    return_buf[0] = module_ret_success;
     raw_hid_send(return_buf, RAW_EPSIZE);
 
     return module_ret_success;
@@ -278,14 +411,17 @@ static int parse_flash_remaining(uint8_t *data, uint8_t length) {
     lfs_ssize_t size = lfs_fs_size(&lfs);
 
     uint8_t return_buf[RAW_EPSIZE] = {0};
-    return_buf[1] = 128 - size;
+    return_buf[0] = module_ret_success;
+    uint32_t remaining = 128 - size;  // Adjust based on actual flash size
+    memcpy(return_buf + 1, &remaining, sizeof(remaining));
+
     raw_hid_send(return_buf, RAW_EPSIZE);
 
-    uprintf("Size: %li\n", 128 - size);
+    uprintf("Size: %li\n", remaining);
     return module_ret_success;
 }
 
-uint8_t current_image_map[(128*128) * LV_COLOR_DEPTH / 8];
+uint8_t current_image_map[(128 * 128) * LV_COLOR_DEPTH / 8];
 
 lv_img_dsc_t current_image = {
     .header.always_zero = 0,
@@ -296,14 +432,26 @@ lv_img_dsc_t current_image = {
     .data = current_image_map,
 };
 
-LV_IMG_DELCARE(current_image);
-
 static int parse_choose_image(uint8_t *data, uint8_t length) {
     uprintf("Choose image\n");
-    struct choose_image_packet *choose_data = (struct choose_image_packet *)data;
-    char *path = (char *)choose_data->directory;
 
-    char full_path[256];  // Adjust the size as needed
+    if (length <= sizeof(struct packet_header)) {
+        uprintf("Insufficient data length\n");
+        return module_ret_invalid_command;
+    }
+
+    uint8_t *path_data = data + sizeof(struct packet_header);
+    size_t path_length = length - sizeof(struct packet_header);
+
+    char path[MAX_PATH_LENGTH];
+    if (path_length >= MAX_PATH_LENGTH - 2) {  // Leave space for "L:" and null terminator
+        uprintf("Path too long\n");
+        return module_ret_invalid_command;
+    }
+    memcpy(path, path_data, path_length);
+    path[path_length] = '\0';
+
+    char full_path[MAX_PATH_LENGTH];
     snprintf(full_path, sizeof(full_path), "L:%s", path);
     uprintf("File path: %s\n", full_path);
 
@@ -322,7 +470,7 @@ static int parse_choose_image(uint8_t *data, uint8_t length) {
     }
 
     if (bytes_read != sizeof(current_image_map)) {
-        uprintf("Warning: Read %d bytes, expected %d bytes\n", bytes_read, sizeof(current_image_map));
+        uprintf("Warning: Read %ld bytes, expected %ld bytes\n", bytes_read, sizeof(current_image_map));
     }
 
     err = close_file(&lfs, &file);
@@ -331,7 +479,7 @@ static int parse_choose_image(uint8_t *data, uint8_t length) {
         return err;
     }
 
-    lv_obj_t * img = lv_img_create(lv_scr_act());
+    lv_obj_t *img = lv_img_create(lv_scr_act());
     lv_img_set_src(img, &current_image);
 
     uint8_t return_buf[RAW_EPSIZE] = {0};
@@ -343,27 +491,36 @@ static int parse_choose_image(uint8_t *data, uint8_t length) {
 uint32_t image_write_pointer = 0;
 static int parse_write_display(uint8_t *data, uint8_t length) {
     uprintf("Write to display\n");
-    struct write_display_packet *write_data = (struct write_display_packet *)data;
 
-    // Calculate the number of bytes to write
-    uint32_t bytes_to_write = length - sizeof(struct packet_header);
-
-    // Write the data to the current_image_map buffer
-    for (uint32_t i = 0; i < bytes_to_write; i++) {
-        current_image_map[image_write_pointer] = write_data->data[i];
-        image_write_pointer++;
-
-        // Check if we've reached the end of the buffer and roll over if necessary
-        if (image_write_pointer >= sizeof(current_image_map)) {
-            image_write_pointer = 0;
-        }
+    if (length <= sizeof(struct packet_header)) {
+        uprintf("No data to write to display\n");
+        return module_ret_invalid_command;
     }
 
-    // Update the display
-    lv_obj_t * img = lv_img_create(lv_scr_act());
-    lv_img_set_src(img, &current_image);
+    uint8_t *write_data = data + sizeof(struct packet_header);
+    uint32_t bytes_to_write = length - sizeof(struct packet_header);
+
+    // Ensure we don't exceed the buffer size
+    if (image_write_pointer + bytes_to_write > sizeof(current_image_map)) {
+        bytes_to_write = sizeof(current_image_map) - image_write_pointer;
+    }
+
+    memcpy(current_image_map + image_write_pointer, write_data, bytes_to_write);
+    image_write_pointer += bytes_to_write;
+
+    // Update the display if the image buffer is full
+    if (image_write_pointer >= sizeof(current_image_map)) {
+        image_write_pointer = 0;  // Reset the pointer
+
+        lv_obj_t *img = lv_img_create(lv_scr_act());
+        lv_img_set_src(img, &current_image);
+    }
 
     uprintf("Wrote %u bytes to display buffer\n", bytes_to_write);
+
+    uint8_t return_buf[RAW_EPSIZE] = {0};
+    return_buf[0] = module_ret_success;
+    raw_hid_send(return_buf, RAW_EPSIZE);
 
     return module_ret_success;
 }
@@ -371,32 +528,23 @@ static int parse_write_display(uint8_t *data, uint8_t length) {
 static int parse_set_time(uint8_t *data, uint8_t length) {
     uprintf("Set time\n");
 
-    struct set_time_packet *time_data = (struct set_time_packet *)data;
+    if (length < sizeof(struct packet_header) + 3) {
+        uprintf("Insufficient data length for time\n");
+        return module_ret_invalid_command;
+    }
+
+    uint8_t *time_data = data + sizeof(struct packet_header);
+
+    uint8_t hour = time_data[0];
+    uint8_t minute = time_data[1];
+    uint8_t second = time_data[2];
 
     // Implement logic to set the system time
     // This might involve updating an RTC or internal time counter
 
-    uprintf("Time set to: %02d:%02d:%02d\n", time_data->hour, time_data->minute, time_data->second);
+    uprintf("Time set to: %02d:%02d:%02d\n", hour, minute, second);
     return module_ret_success;
 }
-
-static module_raw_hid_parse_t* parse_packet_funcs[] = {
-    parse_ls,
-    parse_cd,
-    parse_pwd,
-    parse_rm,
-    parse_mkdir,
-    parse_touch,
-    parse_cat,
-    parse_open,
-    parse_write,
-    parse_close,
-    parse_format_filesystem,
-    parse_flash_remaining,
-    parse_choose_image,
-    parse_write_display,
-    parse_set_time,
-};
 
 int module_raw_hid_parse_packet(uint8_t *data, uint8_t length) {
     int err;
@@ -404,31 +552,45 @@ int module_raw_hid_parse_packet(uint8_t *data, uint8_t length) {
 
     uprintf("Received packet. Parsing command.\r\n");
 
+    if (length < 6 || length > RAW_EPSIZE) {  // Assuming header is 6 bytes
+        uprintf("Invalid packet length\n");
+        return -1;
+    }
+
+    // Manually parse header
+    uint8_t magic_number = data[0];
+    uint8_t command_id = data[1];
+    uint32_t packet_id = *(uint32_t *)(data + 2);
+
     uprintf("Buffer contents: ");
     for (int i = 0; i < length; i++) {
         uprintf("%02X ", data[i]);
     }
     uprintf("\n");
 
-    if (length < sizeof(struct packet_header) || length > RAW_EPSIZE) {
-        uprintf("Invalid packet length\n");
+    if (magic_number != 0x09) {
+        uprintf("Invalid magic number: %02X\n", magic_number);
         return -1;
     }
 
-    struct packet_header *header = (struct packet_header *)(data);
-    uint8_t command_id = header->command_id - id_module_cmd_base;
+    command_id -= id_module_cmd_base;
     uprintf("Command ID: %d\n", command_id);
-    if (command_id >= id_module_cmd_end - id_module_cmd_base) {
+
+    if (command_id >= (sizeof(parse_packet_funcs) / sizeof(parse_packet_funcs[0]))) {
         uprintf("Invalid command ID\n");
         return -1;
     }
 
+    // Call the appropriate parsing function
     err = parse_packet_funcs[command_id](data, length);
+
     if (err < 0) {
         uprintf("Error parsing packet: %d\n", err);
         return_buf[0] = err;
+    } else {
+        return_buf[0] = module_ret_success;
     }
-    return_buf[0] = 0;
+
     raw_hid_send(return_buf, RAW_EPSIZE);
 
     return err;
